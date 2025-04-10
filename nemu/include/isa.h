86b48d5166029a45458b0f3ffa62ab9afa2597e1
tylertan <tylertan@sjtu.edu.cn>
2025-04-10 17:32:36 +0800

    complete difftest and srai

--- nemu/include/isa.h
+++ nemu/include/isa.h
@@ -30,6 +30,7 @@ void init_isa();
 
 // reg
 extern CPU_state cpu;
+extern const char *regs[];
 void isa_reg_display();
 uint32_t isa_reg_str2val(const char *name);
 
--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -17,8 +17,30 @@
 #include <cpu/difftest.h>
 #include "../local-include/reg.h"
 
+#define REGNUM 32
+
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+  bool is_equal = true;
+	if (ref_r->pc != cpu.pc) {
+		is_equal = false;
+	}
+	
+	for (int i = 0; i < REGNUM; i++) 
+		if (gpr(i) != ref_r->gpr[i]) { 
+			printf("DIFF! ref:%3s = 0x%08x while dut: %3s = 0x%08x\n", regs[i], ref_r->gpr[i], regs[i], gpr(i));
+			is_equal = false;
+		}
+
+	if (!is_equal) {
+		printf("ref: pc = %x\n", ref_r->pc);
+		printf("dut: pc = %x\n", cpu.pc);
+		printf("ref:\n");
+		for (int i = 0; i < REGNUM; i++) 
+		 	printf("%-8s 0x%08x %10u\n", regs[i], ref_r->gpr[i], ref_r->gpr[i]);
+		printf("dut:\n");
+	}	
+
+	return is_equal;
 }
 
 void isa_difftest_attach() {
